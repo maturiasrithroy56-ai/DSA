22fd1f12a648d0e0d04158c37e0ea9823865f746
maturiasrithroy56-ai <maturiasrithroy56@gmail.com>
2026-08-20 10:14:09 +0530

    Add files via upload

---
+++ 1c.py
@@ -0,0 +1,13 @@
+n = int(input("Enter number of elements: "))
+
+arr = []
+
+for i in range(n):
+    arr.append(int(input(f"Enter element {i + 1}: ")))
+
+for i in range(n - 1):
+    for j in range(n - 1 - i):
+        if arr[j] > arr[j + 1]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+print("Sorted array:", arr)
---
+++ 2c.py
@@ -0,0 +1,20 @@
+n = int(input("Enter number of elements: "))
+
+arr = []
+
+for i in range(n):
+    arr.append(int(input(f"Enter element {i + 1}: ")))
+
+
+for i in range(1, n):
+
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j -= 1
+
+    arr[j + 1] = key
+
+print("Sorted array:", arr)
